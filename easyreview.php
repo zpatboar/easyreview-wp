b03909cdae6b68ffc759433e1b02984aa130d82d
Zackary P. Boarman <zpbgh@zpb.co>
2017-07-02 15:36:17 -0500

    Initial Wireframe
    This has a working test form with working wp-admin configuration page

---
+++ easyreview.php
@@ -0,0 +1,89 @@
+<?php
+
+/* 
+Plugin Name: EasyReview WordPress Plugin
+Plugin URI: http://www.zpb.me
+Description: Provides a form to submit a review (Demo plugin)
+Author: Boarman Service Company, LLC
+Version: 1.0 Alpha
+Author URI: http://www.boarmanservice.com
+*/
+
+/**
+ * Adds a shortcode to be used on a page
+ */
+function easyreview_form( $atts ){
+    $formvalidated = easyreview_form_validated();
+    if ($formvalidated !== true){
+        ob_start();
+        require_once(__DIR__.'/tmpl/form.php');
+        return ob_get_clean();
+    }
+    
+    return get_option("easyreview_thank_you");
+
+}
+
+add_shortcode( 'easyreview_form', 'easyreview_form' );
+
+function easyreview_form_validated(){
+    $errors = "";
+    if (!isset($_POST['q4oij32r'])){
+        return false;
+    }
+    
+    $name = sanitize_text_field($_POST['your_name']);
+    if ($name == ""){
+        $errors .= "Please enter a Name<br />";
+    }
+    
+    $email = sanitize_email($_POST['your_email']);
+    if ($email == ""){
+        $errors .= "Please enter a valid Email<br />";
+    }
+    
+    if ($_POST['q4oij32r'] != "4" && strtolower($_POST['q4oij32r']) != "four"){
+        $errors .= "Please enter what two plus two is<br />";
+    }
+    
+    if ($errors != ""){
+        return $errors;
+    }
+    
+    return true;
+    
+    
+}
+
+//Admin Submenu
+add_action('admin_menu', 'easyreview_plugin_menu');
+
+function easyreview_plugin_menu(){
+	add_plugins_page('EasyReview', 'EasyReview', 'activate_plugins', 'easyreview', 'easyreview_plugin_admin');
+}
+
+function easyreview_plugin_admin(){
+    if (isset($_POST['apikey'])){
+        update_option("easyreview_apikey", stripslashes(sanitize_text_field($_POST['apikey'])));
+        update_option("easyreview_thank_you", stripslashes(sanitize_textarea_field($_POST['thank_you'])));
+        ?>
+        <div id="setting-error-settings_updated" class="updated settings-error"> 
+            <p><strong>Configuration saved.</strong></p>
+        </div>
+        <?php
+    }
+    ?>
+    <div class="wrap">
+        <h2>EasyReview Configuration</h2>
+        <form method="POST">
+            <label for="apikey">EasyReview API Key</label><br />
+            <input name="apikey" value="<?php echo get_option('easyreview_apikey');?>"><br />
+            <label for="thank_you">Thank You Page Content</label><br />
+            <?php wp_editor(get_option("easyreview_thank_you"),"thank_you"); ?>
+            <p>
+                <?php submit_button("Save"); ?>
+            </p>
+        </form>
+    </div>
+    <?php
+}
---
+++ tmpl/form.php
@@ -0,0 +1,39 @@
+<strong>* Marks a required fields</strong>
+<div style="color: red; line-height: 1.4em; padding-bottom: 10px;">
+    <?php if ($formvalidated !== false){
+        echo $formvalidated;
+    }?>
+</div>
+
+<form method="POST">
+    <fieldset>
+        <legend>Enter Your Contact Information</legend>
+        <label for="your_name">* Your Name</label>
+        <input name="your_name" id="your_name" type="text"  value="<?php echo (isset($_POST['your_name']) ? htmlentities($_POST['your_name']) : "" ); ?>">
+        <label for="your_email">* Your Email</label>
+        <input name="your_email" id="your_email" type="text" value="<?php echo (isset($_POST['your_email']) ? htmlentities($_POST['your_email']) : "" ); ?>">
+        <label for="your_phone">* Your Phone</label>
+        <input name="your_phone" id="your_phone" type="tel" value="<?php echo (isset($_POST['your_phone']) ? htmlentities($_POST['your_phone']) : "" ); ?>">
+    </fieldset>
+    <fieldset>
+        <legend>Enter Your Review</legend>
+        <label for="stars">* On a Scale of 1-5, with 5 being the best, how satisfied are you with us</label>
+        <br />
+        <input type="radio" name="stars" value="1" <?php echo (isset($_POST['stars']) && $_POST['stars'] == 1 ? "checked" : "" ); ?>> 1 &nbsp;
+        <input type="radio" name="stars" value="2" <?php echo (isset($_POST['stars']) && $_POST['stars'] == 2 ? "checked" : "" ); ?>> 2 &nbsp;
+        <input type="radio" name="stars" value="3" <?php echo (isset($_POST['stars']) && $_POST['stars'] == 3 ? "checked" : "" ); ?>> 3 &nbsp;
+        <input type="radio" name="stars" value="4" <?php echo (isset($_POST['stars']) && $_POST['stars'] == 4 ? "checked" : "" ); ?>> 4 &nbsp;
+        <input type="radio" name="stars" value="5" <?php echo (isset($_POST['stars']) && $_POST['stars'] == 5 ? "checked" : "" ); ?>> 5
+        <br />
+        <label for="title">Enter an Optional Title for your Review</label>
+        <input name="title" id="title" type="text" value="<?php echo (isset($_POST['title']) ? htmlentities($_POST['title']) : "" ); ?>">
+        <br />
+        <label for="review">* Enter your review or any comments below</label>
+        <textarea name="review" id="review" style="width: 100%;" rows="5"><?php echo (isset($_POST['review']) ? htmlentities($_POST['review']) : "" ); ?></textarea>
+    </fieldset>
+    <label>* What is two plus two</label>
+    <br />
+    <input name="q4oij32r" type="text">
+    <button type="submit">Submit Review</button>
+</form>
+
